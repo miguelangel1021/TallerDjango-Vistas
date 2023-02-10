fc040c4e9b29eeb930b869aa5b557108899a7d77
miguelangel1021 <69638174+miguelangel1021@users.noreply.github.com>
2023-02-09 21:56:02 -0500

    Cambios

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "monitoringdb",
+        "USER": "miguel",
+        "PASSWORD": "HOLA",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
